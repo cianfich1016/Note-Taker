353c2e81612191f87176e25dfe5ca17a713858b9
Rachel Cianfichi <cianfich@gmail.com>
2021-10-14 18:09:40 -0400

    Working to complete server.js and routes. Still updating errors and determining why notes.html does not show.

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-//Test for Heroku//
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+
+module.exports = app;

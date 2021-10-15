17eeb31847395f3f08aed11282c7a77c73a5e6cf
Rachel Cianfichi <cianfich@gmail.com>
2021-10-15 15:18:32 -0400

    Finalized post and delete routes.

--- routes/notes.js
+++ routes/notes.js
@@ -6,24 +6,37 @@ notes.get('/', (req, res) => {
     read('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.get('/:noteId', (req, res) =>{
-    const noteID = req.params.noteId;
+notes.get('/:id', (req, res) =>{
+    const noteID = req.params.id;
     read('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const final = json.filter((note) => note.noteId == noteID)
+            const final = json.filter((note) => note.id === noteID)
             return final.length > 0
             ? res.json(final) : res.json ("No specific note exists.");
         });
-        writeToFile('.db/db.json', final);
+        
+});
+
+notes.delete('/:id', (req, res) => {
+    const noteID = req.params.id;
+    read('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const final = json.filter((note) => note.id !== noteID);
+
+            writeToFile('./db/db.json', final);
+
+            res.json(`Note ${noteID} has been removed.`);
+        });
 });
 
 notes.post('/', (req, res) => {
     console.log(req.body);
     const { title, text } = req.body;
     if (req.body) {
-        const individualNote = { title, text, noteId: uuidv4(),}; 
-      readAppend(individualNote, './db/db.json');
+        const newNote = { title, text, id: uuidv4(),}; 
+      readAppend(newNote, './db/db.json');
       res.json('Congratulations! Your note has been added!');
     } else {
         res.error ('Note added unsuccessfully.');
